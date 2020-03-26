77c8934eec88a9aa5e37810669d8c5bacb82016c
nasheor <nash.avinashnagarajan@gmail.com>
2020-03-26 22:06:03 +0000

    More changes

--- wallet-web-app/src/components/modals/AttestationForm/index.vue
+++ wallet-web-app/src/components/modals/AttestationForm/index.vue
@@ -3,56 +3,42 @@
         <v-card-title class="grey darken-7">
         Add Attestant
         </v-card-title>
-        <!-- <v-container grid-list-sm>
-        <v-layout
-            row
-            wrap
-        >
-            <v-flex
-            xs12
-            align-center
-            justify-space-between
+        <v-container grid-list-sm>
+            <v-layout
+                row
+                wrap
             >
-            <v-layout align-center>
+                <v-flex
+                xs12
+                align-center
+                justify-space-between
+                >
+                <v-layout align-center>
 
+                    <v-text-field
+                        prepend-icon="mdi-name"
+                        placeholder="Name"
+                        v-model = "name"
+                    ></v-text-field>
+                </v-layout>
+                </v-flex>
+                <v-flex xs12>
                 <v-text-field
-                    prepend-icon="mdi-name"
-                    placeholder="Name"
-                    v-model = "name"
+                    prepend-icon="mdi-email"
+                    placeholder="Email"
+                    v-model = "email"
                 ></v-text-field>
+                </v-flex>
+                <v-flex xs12>
+                <v-text-field
+                    type="tel"
+                    prepend-icon="mdi-phone"
+                    placeholder="(000) 000 - 0000"
+                    v-model = "phone"
+                ></v-text-field>
+                </v-flex>
             </v-layout>
-            </v-flex>
-            <v-flex xs6>
-                <v-text-field
-                    prepend-icon="mdi-domain"
-                    placeholder="File Name"
-                    v-model="caption"
-                ></v-text-field>
-            </v-flex>
-            <v-flex xs6>
-                <v-file-input 
-                    label="File input"
-                    show-size
-                    @change="filePicked">
-                </v-file-input>
-            </v-flex>
-            <v-flex xs12>
-            <v-text-field
-                prepend-icon="mdi-email"
-                placeholder="Email"
-                v-model = "email"
-            ></v-text-field>
-            </v-flex>
-            <v-flex xs12>
-            <v-text-field
-                type="tel"
-                prepend-icon="mdi-phone"
-                placeholder="(000) 000 - 0000"
-                v-model = "phone"
-            ></v-text-field>
-            </v-flex>
-        </v-layout>
-        </v-container> -->
+        </v-container>
         <v-card-actions>
         <v-btn
             text
